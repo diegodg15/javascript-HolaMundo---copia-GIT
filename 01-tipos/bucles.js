d92efdc0c1d6ab4c85e1d1bfb252e809837b4cab
diegoDG <diego.guerrero5dg@outlook.com>
2025-02-04 19:32:13 -0300

    Bucle While

--- 01-tipos/bucles.js
+++ 01-tipos/bucles.js
@@ -5,3 +5,11 @@ for(contador = 0; contador < 10; resultado *=2 ){
     contador++;
 }
 console.log(resultado);
+
+//Bucle While 1 hasta el 5
+let incremento = 0;
+
+while(incremento < 5){
+ incremento++;
+ console.log(incremento);
+}
