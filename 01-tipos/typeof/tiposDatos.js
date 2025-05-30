f848c4bbfb2c06b914fc943c82eea549a9b1b06a
diegoDG <diego.guerrero5dg@outlook.com>
2025-05-30 19:17:01 -0400

    Valores de Typeof

--- 01-tipos/typeof/tiposDatos.js
+++ 01-tipos/typeof/tiposDatos.js
@@ -11,10 +11,24 @@ let x = true;
 let y = false;
 
 // Object:
-const person = {firstName:"John", lastName:"Doe"};
+const person = { firstName: "John", lastName: "Doe" };
 
 // Array object:
 const cars = ["Saab", "Volvo", "BMW"];
 
 // Date object:
 const date = new Date("2022-03-25");
+
+//Valores que puede devolver Typeof
+
+let year = 1990;
+console.log(typeof year);  //  ->  number
+console.log(typeof 1991);  //  ->  number
+
+let name = "Alice";
+console.log(typeof name);  //  ->  string
+console.log(typeof "Bob");  //  ->  string
+
+let typeOfYear = typeof year;
+console.log(typeOfYear);  //  ->  number
+console.log(typeof typeOfYear);  //  ->  string
