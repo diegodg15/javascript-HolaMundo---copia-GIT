3ff892b73684764519ff158a24f6ea3e15c29a82
diegoDG <diego.guerrero5dg@outlook.com>
2025-07-22 13:03:10 -0400

    funciones y sombreado de variables

--- 01-tipos/08-funciones.js
+++ 01-tipos/08-funciones.js
@@ -18,3 +18,40 @@ let suma2=function(n1,n2){
     return n1+ n2;
 }
 console.log(suma2(2,3));
+
+//---------------------------------
+//Impresión console.log
+let n3 = 5;
+function a() {
+    let n3 = 10;
+    return console.log(n3);
+}
+a();             // Imprime 10
+console.log(n3);  // Imprime 5
+console.log(a()); // Imprime 10 y luego undefined-->console.log() = undefined
+// Porque console.log(n) no retorna ningún valor útil—su retorno es undefined. Entonces, la función a() retorna el resultado de console.log(n), que es undefined. 
+
+/**Esto está haciendo:
+ * let resultado = console.log(10); // Esto imprime 10 y guarda "undefined"
+console.log(resultado);          // Imprime undefined
+ */
+//Solucion:
+function a() {
+    let n = 10;
+    return n;
+}
+console.log(a());  // Aquí sí se imprime el valor retornado: 10
+
+
+//TODO LO QUE ESTÁ DESPUÉS DE RETURN, NO LO EJECUTA LA FUNCION
+function a() {
+    let n7 = 10;
+    return n7;           // Aquí termina la función. Nada después se ejecuta.
+    console.log(n7);     // Esto NUNCA se ejecuta.
+}
+
+function a() {
+    let n71 = 10;
+    console.log(n71);     // Esto se ejecuta
+    return n71;           // Aquí termina la función. Nada después se ejecuta.
+}
--- otrosEjerciciosJs/sombreadoVariables.js
+++ otrosEjerciciosJs/sombreadoVariables.js
@@ -1,3 +1,4 @@
+//SOMBREADO DE VARIABLES O SHADOWING
 let contador = 100;
 console.log(contador); //->100
 {
@@ -18,3 +19,14 @@ console.log(contador);//->100
 console.log(contador);//->100
 
 //El console.log al final imprime 100 por que esta declarada nivel global.
+
+const halve = function(n) {
+return n / 2;
+};
+let n = 10;
+console.log(halve(100));
+// → 50
+console.log(n);
+// → 10
+
+
