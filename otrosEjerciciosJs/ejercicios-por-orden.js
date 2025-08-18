9b2d330c782c5b200c557977b3da939bf9086ca1
diegoDG <diego.guerrero5dg@outlook.com>
2025-08-18 19:06:05 -0400

    Ejercicios desde basicos

---
+++ otrosEjerciciosJs/ejercicios-por-orden.js
@@ -0,0 +1,61 @@
+//Ejercicios dgenerales en orden.
+
+//Ejercicios sobre variables:
+/* Declara tres variables: una con var, otra con let y otra con const, e imprime sus valores en consola.
+ */
+{
+    let a = 1;
+    const b = 2;
+    var c = 3;
+    console.log(a,b,c);
+}
+
+/* Usa let para declarar una variable x, asígnale 10, luego cambia su valor a 20 e imprímelo. */
+{
+    let x = 10;
+    x = 20;
+    console.log(x);
+}
+
+/* Intercambia los valores de m y n sin usar una tercera variable (variable auxiliar).
+ */
+{
+    let m = 1;
+    let n = 2;
+
+    m = n;
+    n = m;
+    console.log(m);
+    console.log(n);
+}
+{
+    //Con variable auxiliar:
+    let a = 1;
+    let b = 2;
+    let aux;    
+
+    aux = a;
+    a = b;
+    b = c;
+
+    console.log(a,b)
+}
+
+
+
+
+
+//--------------------------------------------
+
+
+
+
+//--------------------------------------------
+//--------------------------------------------
+//--------------------------------------------
+//--------------------------------------------
+
+
+
+
+
