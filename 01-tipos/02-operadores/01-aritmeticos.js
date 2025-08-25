a437f01489cfeca291b567340a50aede2bed779e
diego <diego.guerrero5dg@outlook.com>
2025-08-25 17:27:27 -0400

    operadores Aritmeticos-Asignacion

--- 01-tipos/02-operadores/01-aritmeticos.js
+++ 01-tipos/02-operadores/01-aritmeticos.js
@@ -1,12 +1,12 @@
 let a = 5;
 let b = 3;
 //Esto es una declaracion
-console.log(a+b, "suma");
-console.log(a-b, "resta");
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
-console.log(a**b);
+console.log(a + b, "suma");
+console.log(a - b, "resta");
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
 //Esto es una expresion
 
 //Operadores de incremento y decremento
@@ -16,7 +16,38 @@ console.log(a++)
 //Imprime 6 y luego 7, este es postincrmeento
 console.log(a);
 
+{
+    console.log(2 + 2 * 2); // -> 6
+    console.log(2 + (2 * 2)); // -> 6
+    console.log((2 + 2) * 2); // -> 8
 
+}
+{
+    const x = 5;
+    const y = 2;
+
+    console.log("addition: ", x + y); // -> 7
+    console.log("subtraction: ", x - y); // -> 3
+    console.log("multiplication: ", x * y); // -> 10
+    console.log("division: ", x / y); // -> 2.5
+    console.log("division remainder :", x % y); // -> 1
+    console.log("exponent: ", x ** y); // -> 25
+}
+
+//OPERADORES ARITMÉTICO UNARIOS
+{
+    let str = "123";
+    let n1 = +str;//También sirve para la conversión rápidad de tipos.
+    let n2 = -str;
+    let n3 = -n2;
+    let n4 = +"abcd";
+    console.log(`${str} : ${typeof str}`); // -> 123 : string
+    console.log(`${n1} : ${typeof n1}`); // -> 123 : number
+    console.log(`${n2} : ${typeof n2}`); // -> -123 : number
+    console.log(`${n3} : ${typeof n3}`); // -> 123 : number
+    console.log(`${n4} : ${typeof n4}`); // -> NaN : number
+
+}
 
 
 
--- 01-tipos/02-operadores/02-asignacion.js
+++ 01-tipos/02-operadores/02-asignacion.js
@@ -1,7 +1,21 @@
 let a = 5;
 let b = a + 5;
 
-a = a +5;
-a +=5;
+a = a + 5;
+a += 5;
 
 console.log(a);
+{
+    const name = "Alice";
+    console.log(name); // -> Alice
+}
+{
+    let year = 2050;
+    let newYear = year = 2051;//Si aparecen varios operadores de asignación en una secuencia, se aplica el orden de derecha a izquierda.
+
+    {//Es lo mismo que arriba ^
+        let year = 2050;
+        year = 2051;
+        let newYear = year;
+    }
+}
