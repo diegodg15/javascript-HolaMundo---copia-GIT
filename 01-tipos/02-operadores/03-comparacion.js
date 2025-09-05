d476963b627cb4c54b5cec6e56366e019fa14c13
diego <diego.guerrero5dg@outlook.com>
2025-09-04 22:22:44 -0400

    feat: Agregar codigo js operadores de comparación.

--- 01-tipos/02-operadores/03-comparacion.js
+++ 01-tipos/02-operadores/03-comparacion.js
@@ -4,7 +4,7 @@ console.log(a >= 5);//True
 console.log(a < 5);//False
 console.log(a <= 5);//True
 
-//Operadores de igualdad
+//Operadores de igualdad:
 
 console.log(a == 10);//True
 console.log(a != 10);//False
@@ -45,4 +45,74 @@ variable = true;        // Y ahora es de tipo boolean
 
 /* Los operadores de comparación se utilizan para comprobar la igualdad o desigualdad de valores. Todos son binarios y devuelven un valor lógico que representa el resultado de la comparación.verdadero o FALSO.
  */
+{
+    console.log(10 === 5); // -> false
+    console.log(10 === 10); // -> true
+    console.log(10 === 10n); // -> false
+    console.log(10 === "10"); // -> false
+    console.log("10" === "10"); // -> true
+    console.log("Alice" === "Bob"); // -> false
+    console.log(0 === false); // -> false
+    console.log(undefined === false); // -> false
+}
 
+{
+    console.log(10 == 5); // -> false
+    console.log(10 == 10); // -> true
+    console.log(10 == 10n); // -> true
+    console.log(10 == "10"); // -> true
+    console.log("10" == "10"); // -> true
+    console.log("Alice" == "Bob"); // -> false
+    console.log(0 == false); // -> true
+    console.log(undefined == false); // -> false
+    console.log(NaN == NaN); // -> false
+}
+
+{
+    console.log(10 !== 5); // -> true
+    console.log(10 !== 10); // -> false
+    console.log(10 !== 10n); // -> true
+    console.log(10 !== "10"); // -> true
+    console.log("10" !== "10"); // -> false
+    console.log("Alice" !== "Bob"); // -> true
+    console.log(0 !== false); // -> true
+    console.log(undefined !== false); // -> true
+    console.log(10 != 5); // -> true
+    console.log(10 != 10); // -> false
+    console.log(10 != 10n); // -> false
+    console.log(10 != "10"); // -> false
+    console.log("10" != "10"); // -> false
+    console.log("Alice" != "Bob"); // -> true
+    console.log(0 != false); // -> false
+    console.log(undefined != false); // -> true
+    console.log(NaN != NaN); // -> true
+}
+
+{
+    console.log(10 > 100); // -> false
+    console.log(101 > 100); // -> true
+    console.log(101 > "100"); // -> true
+
+    console.log(101 < 100); // -> false
+    console.log(100n < 102); // -> true
+    console.log("10" < 20n); // -> true
+
+    console.log(101 <= 100); // -> false
+    console.log(10 >= 10n); // -> true
+    console.log("10" <= 20); // -> true
+}
+
+{
+    console.log("b" > "a"); // -> true
+    console.log("a" > "B"); // -> true
+    console.log("B" > "A"); // -> true
+    console.log("A" > "4"); // -> true
+    console.log("4" > "1"); // -> true
+
+    console.log("ab1" < "ab4"); // -> true
+    console.log("ab4" < "abA"); // -> true
+    console.log("abB" < "aba"); // -> true
+    console.log("aba" < "abb"); // -> true
+
+    console.log("ab" < "ab4"); // -> true
+}
